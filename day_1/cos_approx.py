28c8a6b698e4e669193e7b011787a614d27248d9
Ahmed Issawi <aissawi@unb.ca>
2022-07-19 08:08:51 -0600

    Day 1 project

--- day_1/cos_approx.py
+++ day_1/cos_approx.py
@@ -8,33 +8,24 @@ __email__ = 'aissawi@unb.ca'
 
 from math import factorial
 from math import pi
-from scipy.integrate import quad
-
 
 def cos_approx(x, accuracy=10):
     """
+    The Maclaurin series expansion for cos(x)
+    - X  integer number 
+    Independent variable 
+    - Accuracy integer
+    series of numbers identify the range we will substitute in the equation
     
+    Our result is the Sum of the list we created from the calculation of the body equation 
+    which is the range of the cosin values. 
     """
-    # powers = [n for n in range(accuracy)]
     
-    # calculated_results = [ (((-1)**n) * x ** (2*n)) / (factorial(2 * n)) for n in range(powers)]
-    
-    
-    # return sum(calculated_list)
-    
-    power = [n for n in range(accuracy)]
-    upper = (-1) ** power
-    lower = (2 * factorial(power))
-    right = x ** (2 * power)
-    result_BS = (upper * right) / lower
-    calculated_list = []
-    return sum(calculated_list) 
+    return sum([ (((-1)**n) * x ** (2*n)) / (factorial(2 * n)) for n in range(accuracy)]) 
 
-
-
-# Will only run if this is run from command line as opposed to imported
-# if __name__ == '__main__':  # main code block
-#     print("cos(0) = ", cos_approx(0))
-#     print("cos(pi) = ", cos_approx(pi))
-#     print("cos(2*pi) = ", cos_approx(2*pi))
-#     print("more accurate cos(2*pi) = ", cos_approx(2*pi, accuracy=50))
+#Will only run if this is run from command line as opposed to imported
+if __name__ == '__main__':  # main code block
+    print("cos(0) = ", cos_approx(0))
+    print("cos(pi) = ", cos_approx(pi))
+    print("cos(2*pi) = ", cos_approx(2*pi))
+    print("more accurate cos(2*pi) = ", cos_approx(2*pi, accuracy=50))
